e974f80791fbd10bf8d9140b12d9315c25aeb3dd
kotuw <46974369+KotUW@users.noreply.github.com>
2023-04-08 22:57:06 +0530

    Doesn't do anython

--- home.nix
+++ home.nix
@@ -70,17 +70,17 @@
             lsp-nix = "nix shell nixpkgs#nil";
             lsp-py = "nix shell nixpkgs#python310Packages.python-lsp-server";
         };
-        plugins = [
-            {
-                name = "fish-ssh-agent";
-                src = pkgs.fetchFromGitHub {
-                    owner = "danhper";
-                    repo = "fish-ssh-agent";
-                    rev = "fd70a2afdd03caf9bf609746bf6b993b9e83be57";
-                    sha256 = "sha256-e94Sd1GSUAxwLVVo5yR6msq0jZLOn2m+JZJ6mvwQdLs=";
-                };
-            }
-        ];
+        # plugins = [
+        #     {
+        #         name = "fish-ssh-agent";
+        #         src = pkgs.fetchFromGitHub {
+        #             owner = "danhper";
+        #             repo = "fish-ssh-agent";
+        #             rev = "fd70a2afdd03caf9bf609746bf6b993b9e83be57";
+        #             sha256 = "sha256-e94Sd1GSUAxwLVVo5yR6msq0jZLOn2m+JZJ6mvwQdLs=";
+        #         };
+        #     }
+        # ];
     };
 
     programs.zoxide = {
