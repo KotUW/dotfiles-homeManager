e8aec0fcb1c4520557241988221006b236dce3fd
kotuw <46974369+KotUW@users.noreply.github.com>
2023-04-09 10:44:04 +0530

    It was workin maybe.

--- home.nix
+++ home.nix
@@ -68,17 +68,17 @@
             py = "nix run nixpkgs#python310Packages.bpython";
             top = "btm -b";
         };
-        # plugins = [
-        #     {
-        #         name = "fish-ssh-agent";
-        #         src = pkgs.fetchFromGitHub {
-        #             owner = "danhper";
-        #             repo = "fish-ssh-agent";
-        #             rev = "fd70a2afdd03caf9bf609746bf6b993b9e83be57";
-        #             sha256 = "sha256-e94Sd1GSUAxwLVVo5yR6msq0jZLOn2m+JZJ6mvwQdLs=";
-        #         };
-        #     }
-        # ];
+        plugins = [
+            {
+                name = "fish-ssh-agent";
+                src = pkgs.fetchFromGitHub {
+                    owner = "danhper";
+                    repo = "fish-ssh-agent";
+                    rev = "fd70a2afdd03caf9bf609746bf6b993b9e83be57";
+                    sha256 = "sha256-e94Sd1GSUAxwLVVo5yR6msq0jZLOn2m+JZJ6mvwQdLs=";
+                };
+            }
+        ];
     };
 
     programs.zoxide = {
