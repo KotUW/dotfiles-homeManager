cdfbeed4b160f68c5ac7d1709f35ded72c3a7b12
kotuw <46974369+KotUW@users.noreply.github.com>
2023-05-06 09:49:59 +0530

    Edit:xplr changes. Added nuke (some conf still required)

--- df/xplr/init.lua
+++ df/xplr/init.lua
@@ -1,12 +1,12 @@
 version = '0.20.1'
 local xplr = xplr
+local home = os.getenv("HOME")
+local key = xplr.config.modes.builtin.default.key_bindings.on_key
 
 -- xpm (xplr) plugin manager
 
-local home = os.getenv("HOME")
 local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
 local xpm_url = "https://github.com/dtomvan/xpm.xplr"
-
 package.path = package.path
   .. ";"
   .. xpm_path
@@ -22,7 +22,6 @@ os.execute(
     xpm_path
   )
 )
-
 -- set up plugin manager
 require("xpm").setup({
   plugins = {
@@ -31,10 +30,22 @@ require("xpm").setup({
     { name = 'sayanarijit/fzf.xplr' },
     -- 'sayanarijit/zentable.xplr',
     'prncss-xyz/icons.xplr',
+    -- xplr file viewer and opener
+    'Junker/nuke.xplr'
   },
   auto_install = true,
   auto_cleanup = true,
 })
 
-xplr.config.general.enable_recover_mode=true
+-- Setup plugins --
+-- = nuke
+require("nuke").setup()
+key.v = {
+  help = "nuke",
+  messages = {"PopMode", {SwitchModeCustom = "nuke"}}
+}
+key["f3"] = xplr.config.modes.custom.nuke.key_bindings.on_key.v
+key["enter"] = xplr.config.modes.custom.nuke.key_bindings.on_key.o
 
+-- user config
+xplr.config.general.enable_recover_mode=true
