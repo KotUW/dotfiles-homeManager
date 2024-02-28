a79d61e0bb160ee39c280d14d84077832da1e9da
kotuw <46974369+KotUW@users.noreply.github.com>
2024-02-28 22:56:38 +0000

    Added home.nix and kak-lsp conf (legacy)

---
+++ home.nix
@@ -0,0 +1,226 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "evil";
+  home.homeDirectory = "/home/evil";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+
+  programs.git = {
+    enable = true;
+    userName = "kotuw";
+    userEmail = "46974369+KotUW@users.noreply.github.com";
+    delta.enable = true;
+    extraConfig = {
+      init.defaultBranch = "main";   
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ecdsa.pub";
+      core.editor = "hx";
+    };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+      # fileWidgetCommand = "fd --type f";
+  };
+
+  programs.fish = {
+    enable = true;
+    shellAbbrs = {
+      q = "exit";
+      c = "clear";
+      cat = "bat";
+      lg = "gitui";
+    };
+  };
+  
+  programs.helix = {
+    enable = false; # Enable after you change below settings with yours
+    settings = {
+      theme = "noctis_bordo";
+      editor = {
+        cursorline = true;
+        color-mode = true;
+        bufferline = "multiple";
+        auto-save = true;
+        auto-format = true;
+      };
+
+      editor.lsp = {
+        display-messages = true;
+        dispaly-inlay-hints = true;
+      };
+
+      editor.soft-wrap.enable = true;
+      editor.indent-guides.render = true;
+      keys.normal = {
+        W = "expand_selection";
+        "A-/" = "toggle_comments";
+        space.w = ":w";
+        space.x = ":wq";
+        esc = [ "collapse_selection" "keep_primary_selection" ];
+      };
+      keys.insert = {
+      };
+    };
+    languages = {
+      language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      } 
+      {
+        name = "text-languageTool";
+        language-servers = [{ name = "ltex";} ];  
+        file-types = ["md" "txt" "adoc"];
+        scope = "text.languageTool";
+      }
+      ];
+      language-server.ltex = {
+        command = "${pkgs.ltex-ls}/bin/ltex-ls";
+      }; 
+    };
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
+    };
+  };  # Helix Conf
+
+  programs.bat = {
+    enable = true;
+    config = {
+    theme = "Dracula";
+    };
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.broot = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+  
+  programs.eza = {
+      enable = true;
+      enableAliases = true;
+  };
+    programs.zellij = {
+        enable = true;
+        # enableFishIntegration = true; doesn't work for unknown reasons.
+        settings = {
+            copy_clipboard = "primary";
+            scrollback_editor = "hx";
+        };
+    };
+    
+  programs.gitui = {
+    enable = true;
+    theme = ''(
+          selected_tab: Reset,
+          command_fg: White,
+          selection_bg: DarkGray,
+          selection_fg: White,
+          cmdbar_bg: Black,
+          cmdbar_extra_lines_bg: Black,
+          disabled_fg: DarkGray,
+          diff_line_add: Green,
+          diff_line_delete: Red,
+          diff_file_added: LightGreen,
+          diff_file_removed: LightRed,
+          diff_file_moved: LightMagenta,
+          diff_file_modified: Yellow,
+          commit_hash: Magenta,
+          commit_time: LightCyan,
+          commit_author: Green,
+          danger_fg: Red,
+          push_gauge_bg: Blue,
+          push_gauge_fg: Reset,
+          tag_fg: LightMagenta,
+          branch_fg: LightYellow,
+      )
+    '';
+    keyConfig = ''(
+        status_reset_item: Some((code: Char('d'), modifiers: (bits: 0,),)),
+      )
+    '';
+  };
+
+  home.packages = [
+    pkgs.just
+    pkgs.rustup
+    pkgs.go
+    pkgs.nil # make maybe helix-extra-packages?
+    pkgs.gopls# make maybe helix-extra-packages?
+    pkgs.bat
+    pkgs.pipx
+    pkgs.ltrace
+    pkgs.catgirl
+    pkgs.pijul
+    pkgs.hyperfine
+    pkgs.hexyl
+    pkgs.dt
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/evil/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "hx";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
