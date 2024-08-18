59152dca32c1529f7b624adfc37167d6dfb47edd
Koustubh Saxena <46974369+KotUW@users.noreply.github.com>
2024-08-18 01:43:52 +0100

    kitty conf update

--- home.nix
+++ home.nix
@@ -222,7 +222,6 @@
     pkgs.zellij
     pkgs.zoxide
     pkgs.watchexec
-    pkgs.yazi
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
